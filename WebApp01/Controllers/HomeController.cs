378b3e38fc54084f83c568bd17c2601ec7c42273
wsizim <welson.sizim@gmail.com>
2016-02-08 12:31:50 -0200

    Release 001

---
+++ WebApp01/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using WebApp01.Models;
+
+namespace WebApp01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            var registro = new RegistroModel();
+            return View(registro);
+        }
+
+        [HttpPost]
+        public ActionResult Registro(RegistroModel registro)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Obrigado", registro);
+            }
+            else
+            {
+                return View(registro);
+            }
+        }
+
+        public ActionResult Obrigado(RegistroModel registro)
+        {
+            return View(registro);
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+    }
+}
